779f285c11f5d9bea3f5a2cfb34289b5335f10c7
Ychika <shamoji4804@gmail.com>
2023-04-12 22:03:43 +0900

    update: os.environ -> os.environ.get

--- app/config.py
+++ app/config.py
@@ -2,15 +2,11 @@ from os      import environ
 from os.path import join, dirname
 from dotenv  import load_dotenv
 
-#### load shell env
+#### load .env
 env = join(dirname(__file__), ".env")
 load_dotenv(env)
 
-#### shell env as a value
-if 'APP_ENV' in environ:
-  APP_ENV = environ['APP_ENV']
-else:
-  APP_ENV = "undefined"
-
-GOOGLE_CLIENT_ID  = environ['GOOGLE_CLIENT_ID']
-GOOGLE_CLIENT_SEC = environ['GOOGLE_CLIENT_SEC']
+#### Reads shell variables (returns None if not defined)
+APP_ENV           = environ.get('APP_ENV')
+GOOGLE_CLIENT_ID  = environ.get('GOOGLE_CLIENT_ID')
+GOOGLE_CLIENT_SEC = environ.get('GOOGLE_CLIENT_SEC')
